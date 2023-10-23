9c17c65cf0dbf281ddc562756b733615b58d3d1d
Kristin Ly <krily@ucdavis.edu>
2023-10-23 15:36:23 -0700

    madlibs

---
+++ studio1/script.js
@@ -0,0 +1,22 @@
+(function () {
+
+    'use strict';
+
+    document.querySelector('.open').addEventListener('click',function(event){
+        event.preventDefault();
+        document.querySelector('#overlay').className= 'showing';
+    });
+    
+    document.querySelector('.close').addEventListener('click',function(event){
+        event.preventDefault();
+        document.querySelector('#overlay').className=
+        'hidden';
+    });
+
+    document.addEventListener('keydown',function(event){
+        if(event.key === 'Escape'){
+            document.querySelector('#overlay').className= 'hidden';
+        }
+    });
+
+})();
